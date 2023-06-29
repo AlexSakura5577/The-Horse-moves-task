04568c7d92b8b7814f277bc28c295cef7888fad7
Manticora <999manticora999@gmail.com>
2023-06-29 12:28:54 +0300

    feat: the horse walks correctly

--- js/app.js
+++ js/app.js
@@ -59,54 +59,76 @@ function nextStep() {
         if (!vars[i] || vars[i].classList.contains('set')) {
             vars.splice(i, 1);
         }
-    }
-
-    let nextArr = [];
-    function whatToDoNext() {
-        for (let i = 0; i < vars.length; i++) {
-            let nextX = vars[i].getAttribute('posX');
-            let nextY = vars[i].getAttribute('posY');
-            let nextVars = [document.querySelector('[posX="' + (+nextX + 1) + '"][posY="' + (+nextY + 2) + '"]'),
-            document.querySelector('[posX="' + (+nextX + 1) + '"][posY="' + (+nextY - 2) + '"]'),
-            document.querySelector('[posX="' + (+nextX - 1) + '"][posY="' + (+nextY - 2) + '"]'),
-            document.querySelector('[posX="' + (+nextX - 2) + '"][posY="' + (+nextY - 1) + '"]'),
-            document.querySelector('[posX="' + (+nextX - 2) + '"][posY="' + (+nextY + 1) + '"]'),
-            document.querySelector('[posX="' + (+nextX - 1) + '"][posY="' + (+nextY + 2) + '"]'),
-            document.querySelector('[posX="' + (+nextX + 2) + '"][posY="' + (+nextY + 1) + '"]'),
-            document.querySelector('[posX="' + (+nextX + 2) + '"][posY="' + (+nextY - 1) + '"]')
-            ];
-
-            for (let i = nextVars.length - 1; i >= 0; i--) {
-                if (!nextVars[i] || nextVars[i].classList.contains('set')) {
-                    nextVars.splice(i, 1);
-                }
-            }
-
-            nextArr.push(nextVars.length);
-        };
-        return nextArr;
     };
 
-    // console.log(whatToDoNext());
-    nextArr = whatToDoNext();
+    if (vars.length > 0) {
+        let nextArr = [];
+        function whatToDoNext() {
+            for (let i = 0; i < vars.length; i++) {
+                let nextX = vars[i].getAttribute('posX');
+                let nextY = vars[i].getAttribute('posY');
+                let nextVars = [document.querySelector('[posX="' + (+nextX + 1) + '"][posY="' + (+nextY + 2) + '"]'),
+                document.querySelector('[posX="' + (+nextX + 1) + '"][posY="' + (+nextY - 2) + '"]'),
+                document.querySelector('[posX="' + (+nextX - 1) + '"][posY="' + (+nextY - 2) + '"]'),
+                document.querySelector('[posX="' + (+nextX - 2) + '"][posY="' + (+nextY - 1) + '"]'),
+                document.querySelector('[posX="' + (+nextX - 2) + '"][posY="' + (+nextY + 1) + '"]'),
+                document.querySelector('[posX="' + (+nextX - 1) + '"][posY="' + (+nextY + 2) + '"]'),
+                document.querySelector('[posX="' + (+nextX + 2) + '"][posY="' + (+nextY + 1) + '"]'),
+                document.querySelector('[posX="' + (+nextX + 2) + '"][posY="' + (+nextY - 1) + '"]')
+                ];
 
-    let k = nextArr.length;
-    let min = nextArr[0];
-    var index = 0;
-    while (k--) {
-        if (nextArr[k] < min) {
-            min = nextArr[k];
-            index = k;
+                for (let i = nextVars.length - 1; i >= 0; i--) {
+                    if (!nextVars[i] || nextVars[i].classList.contains('set')) {
+                        nextVars.splice(i, 1);
+                    }
+                }
+
+                nextArr.push(nextVars.length);
+            };
+            return nextArr;
+        };
+
+        // console.log(whatToDoNext());
+        nextArr = whatToDoNext();
+
+        let k = nextArr.length;
+        let min = nextArr[0];
+        var index = 0;
+        while (k--) {
+            if (nextArr[k] < min) {
+                min = nextArr[k];
+                index = k;
+            }
+        }
+
+        step++;
+        document.querySelector('.current').classList.remove('current');
+
+        vars[index].classList.add('current');
+        vars[index].classList.add('set');
+        vars[index].innerHTML = step;
+        currentX = vars[index].getAttribute('posX');
+        currentY = vars[index].getAttribute('posY');
+
+        if (step == 64) {
+            clearInterval(interval);
+            setTimeout(() => {
+                alert('Успешно');
+                document.location.reload(true);
+            }, 200);
         }
     }
 
+    // если конь зашёл в тупик
+    else {
+        alert('Тупик!')
+        document.location.reload(true);
+    };
 };
-nextStep();
 
-document.querySelector('.current').classList.remove('current');
-
-vars[index].classList.add('current');
-vars[index].classList.add('set');
+let interval = setInterval(() => {
+    nextStep();
+}, 200);
 
 
 
